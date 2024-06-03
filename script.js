fc93b90ef7cfba4dccdaae70d5684d0e8ac8fa0d
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-06-03 15:06:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //your JS code here. If required.
 let timerDisplay=document.getElementById("timer");
+let currentTime=new Date();
 setInterval(
-	function(){timerDisplay.innerText=new Date()},1000)
+	function(){
+		currentTime=new Date();
+		
+		timerDisplay.innerText=currentTime.toLocaleString()},1000)
